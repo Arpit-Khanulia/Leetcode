9ab45ef566e840e7728105030c2a1b5a52760539
Arpit-Khanulia <88695197+Arpit-Khanulia@users.noreply.github.com>
2022-08-01 05:22:08 -0700

    Time: 47 ms (64.47%), Space: 30.9 MB (63.96%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -9,46 +9,36 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    int countNodes(TreeNode* root) {
-        
-       return count(root);
-        
+    int countNodes(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+
+        int lh = lheight(root->left, 1);
+        int rh = rheight(root->right, 1);
+
+        if (lh == rh)
+            return pow(2, lh) - 1;
+        else
+            return 1 + countNodes(root->left) + countNodes(root->right);
     }
-    
-    int count(TreeNode * root){
-        if(root == NULL)
-            return 0 ;
-        
-        int lh = lheight(root->left,1);
-        int rh = rheight(root->right,1);
-        
-        // cout<<"root = "<<root->val<<" left = "<<lh<<" right = "<<rh<<endl;
-        
-        if(lh == rh)
-            return pow(2,lh) -1;
-        else return 1 + count(root->left) + count(root->right);
-            
-    }
-    
-    int lheight(TreeNode * root,int h){
-        
-        if(root == NULL)
+
+    int lheight(TreeNode *root, int h)
+    {
+        if (root == NULL)
             return h;
-        
-        return lheight(root->left,h+1);
-        
-        
+
+        return lheight(root->left, h + 1);
     }
-    
-    int rheight(TreeNode * root,int h){
-        
-        if(root == NULL)
+
+    int rheight(TreeNode *root, int h)
+    {
+        if (root == NULL)
             return h;
-        
-        return rheight(root->right,h+1);
-        
-        
+
+        return rheight(root->right, h + 1);
     }
 };

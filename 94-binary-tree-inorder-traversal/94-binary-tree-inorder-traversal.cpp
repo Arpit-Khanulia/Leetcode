3b1ccaf61ca12833ccdb516e7365b5a1d0ec1d77
Arpit-Khanulia <88695197+Arpit-Khanulia@users.noreply.github.com>
2022-09-03 05:59:39 -0700

    Time: 0 ms (100.00%), Space: 8.4 MB (39.60%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -9,45 +9,51 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    vector<int> inorderTraversal(TreeNode* root) {
-        
-        vector<int>inorder;
-        
-        TreeNode* cur = root;
-        
-        while(cur != NULL){
-            
-            if(cur->left == NULL)
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+
+        vector<int> inorder;
+        if (root == NULL)
+            return inorder;
+
+        while (root != NULL) {
+
+            if (root->left == NULL)
             {
-                inorder.push_back(cur->val);
-                cur = cur->right;
+                inorder.push_back(root->val);
+                root = root->right;
             }
-            else {
-                
-                
-                TreeNode * prev = cur->left;
-                
-                while(prev->right && prev->right != cur)
-                    prev =prev->right;
-                
-                
-                if(prev->right == NULL)
+
+            else
+            {
+
+                TreeNode *prev = root->left;
+
+                while (prev->right && prev->right != root)
                 {
-                    prev->right = cur;
-                    cur = cur->left;
+                    prev = prev->right;
                 }
-                
-                else if(prev ->right !=NULL){
+
+                if (prev->right == NULL)
+                {
+                    prev->right = root;
+                    root = root->left;
+                }
+
+                else if (prev->right != NULL)
+                {
                     prev->right = NULL;
-                    inorder.push_back(cur->val);
-                    cur = cur->right;
+                    inorder.push_back(root->val);
+                    root = root->right;
+                    
                 }
             }
+            
         }
-        
+
         return inorder;
-        
     }
 };

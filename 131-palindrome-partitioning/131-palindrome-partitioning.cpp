9396ae6fb896abd1d305e67377a6a5825e2a7ddd
Arpit-Khanulia <88695197+Arpit-Khanulia@users.noreply.github.com>
2022-09-26 18:59:02 +0000

    Time: 609 ms (5.50%), Space: 144.7 MB (17.44%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        
+        vector<string>ds;
+        vector<vector<string>>ans;
+        find(0,ds,ans,s);
+        return ans;
+    }
+    
+    
+    void find(int index, vector<string>ds, vector<vector<string>>&ans,string s){
+        
+        if(index == s.size())
+        {
+            ans.push_back(ds);
+            return ;
+        }
+        
+        
+        for(int i=index;i<s.size();i++)
+        {
+            if(ispallindrome(s,index,i))
+            {
+                ds.push_back(s.substr(index,i-index+1));
+            
+                find(i+1,ds,ans,s);
+                ds.pop_back();            
+                
+            }
+
+        }
+    }
+    
+    bool ispallindrome(string s,int start,int end){
+        
+        while(start<=end)
+            if(s[start++] != s[end--])
+                return false;
+        
+        return true;
+    }
+};

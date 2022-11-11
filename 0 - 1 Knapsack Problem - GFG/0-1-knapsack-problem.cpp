5d269e721bcc6d6afa63629878001bf2ba584c62
Arpit-Khanulia <88695197+Arpit-Khanulia@users.noreply.github.com>
2022-11-11 18:07:11 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -72,27 +72,60 @@ class Solution
         // }
     
     
-    int find(int w , int wt[], int val[], int n, vector<vector<int>>&dp){
+    // int find(int w , int wt[], int val[], int n, vector<vector<int>>&dp){
         
-        if(n == 0 || w == 0) return 0;
+    //     if(n == 0 || w == 0) return 0;
         
-        if(dp[w][n] != -1) return dp[w][n];
+    //     if(dp[w][n] != -1) return dp[w][n];
         
-        if(wt[n-1] <= w )
-        {
-            int include  = val[n-1] + find(w-wt[n-1],wt,val,n-1,dp);
-            int notinclude = find(w,wt,val,n-1,dp);
+    //     if(wt[n-1] <= w )
+    //     {
+    //         int include  = val[n-1] + find(w-wt[n-1],wt,val,n-1,dp);
+    //         int notinclude = find(w,wt,val,n-1,dp);
             
-            return dp[w][n] = max(include,notinclude);
-        }
+    //         return dp[w][n] = max(include,notinclude);
+    //     }
         
-        return dp[w][n] = find(w,wt,val,n-1,dp);
-    }
+    //     return dp[w][n] = find(w,wt,val,n-1,dp);
+    // }
+    // int knapSack(int W, int wt[], int val[], int n) {
+        
+    //     vector<vector<int>>dp(W+5,vector<int>(n+5,-1));
+    //     return find(W,wt,val,n,dp);
+        
+    // }
+    
     int knapSack(int W, int wt[], int val[], int n) {
         
-        vector<vector<int>>dp(W+5,vector<int>(n+5,-1));
-        return find(W,wt,val,n,dp);
+        vector<vector<int>>dp(n+5,vector<int>(W+5));
         
+        
+        for(int i =0 ; i <= n; i++)
+        for(int j =0 ; j <= W; j++)
+        {
+            if(i == 0 || j== 0)
+            dp[n][W] = 0;
+            
+            // if(j==0)
+            // dp[W][n] = 0;
+        }
+        
+        for(int i =1; i<= n;i++)
+        for(int j =1; j<= W;j++)
+        {
+            
+            if(wt[i-1] <= j)
+            {
+                
+                dp[i][j] = max( val[i-1] + dp[i-1][ j - wt[i-1] ] , dp[i-1][j] );
+                
+            }
+            
+            else dp[i][j] =  dp[i-1][j];
+        }
+        
+        
+        return dp[n][W];
     }
 };
 

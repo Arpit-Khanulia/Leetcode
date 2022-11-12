301243af83c58d8cc44d0eb1f9ea46b04de4fd9a
Arpit-Khanulia <88695197+Arpit-Khanulia@users.noreply.github.com>
2022-11-12 14:19:52 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -6,26 +6,26 @@ using namespace std;
 class Solution {
   public:
   
-    long long find(int n, int target,int coins[],vector<vector<long long >>&dp){
+    long long find(int coins[], int sum, vector<vector<long long>>&dp,long long n){
         
-        if(n ==0)
-        {
-            if(target == 0) return 1;
+        if(n ==0){
+            if(sum ==0) return 1;
             return 0;
         }
         
-        if(dp[n][target] != -1) return dp[n][target];
+        if(dp[n][sum] != -1) return dp[n][sum];
         
-        if(coins[n-1] <= target)
-        return dp[n][target] = find(n,target - coins[n-1],coins,dp) + find(n-1,target,coins,dp);
-        return dp[n][target] = find(n-1,target,coins,dp);
+        
+        if(coins[n-1] <= sum )
+        return dp[n][sum]=  find(coins,sum-coins[n-1],dp,n) + find(coins,sum,dp,n-1);
+        return dp[n][sum]=  find(coins,sum,dp,n-1);
     }
     long long int count(int coins[], int N, int sum) {
 
         // code here.
         
-        vector<vector<long long >>dp(N+1,vector<long long>(sum+1,-1));
-        return find(N,sum,coins,dp);
+        vector<vector<long long>>dp(N+5,vector<long long >(sum+5,-1));
+        return  find(coins,sum,dp,N);
     }
 };
 

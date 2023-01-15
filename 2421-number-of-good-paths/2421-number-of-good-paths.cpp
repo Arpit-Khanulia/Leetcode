ba7749ff4c046edfa7579cb74987553b705e8506
Arpit-Khanulia <88695197+Arpit-Khanulia@users.noreply.github.com>
2023-01-15 09:05:32 -0800

    Time: 922 ms (58.97%), Space: 199.5 MB (79.79%) - LeetHub

--- 2421-number-of-good-paths/2421-number-of-good-paths.cpp
+++ 2421-number-of-good-paths/2421-number-of-good-paths.cpp
@@ -1,70 +1,104 @@
-struct DSU {
-    vector<int> size;
-    vector<int> par;
+
+class Solution {
+public:
     
-    DSU (int n) {
-        size.resize(n, 1);
-        par.resize(n);
-        for (int j = 0; j < n; j ++) par[j] = j;
+    int find(int a,vector<int>&parent){
+        
+        if(a == parent[a]) return a;
+        return parent[a] = find(parent[a],parent);
+        
     }
     
-    int Leader (int x) {
-        if (x == par[x]) return x;
-        return (par[x] = Leader(par[x]));
-    }
-    
-    void merge (int x, int y) {
-        x = Leader(x);
-        y = Leader(y);
+    void unionn(int a, int b,vector<int>&parent,vector<int>&size){
+        
+        int x = find(a,parent);
+        int y = find(b,parent);
+        
+        
         if (x == y) return;
         if (size[x] > size[y]) swap (x, y);
         
         size[y] += size[x];
-        par[x] = y;
+        parent[x] = y;
+    }
+    
+    int numberOfGoodPaths(vector<int>& vals, vector<vector<int>>& edges) {
+        
+        int n = vals.size();
+        vector<int>size(n,1);
+        vector<int>parent(n);
+        
+        for(int i=0; i<n;i++)
+            parent[i] =i;
+        
+        map<int,vector<int>>m;
+        
+        vector<int>adj[n];
+        
+        for(int i=0; i<edges.size();i++){
+            
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+        }
+        
+        for(int i =0; i<vals.size();i++){
+            m[vals[i]].push_back(i);
+        }
+        
+        vector<int>active(n);
+        int result = vals.size();
+        
+        
+        for(auto i: m){
+            for(int j : i.second){
+                
+                for(int k: adj[j]){
+                    if(active[k]) unionn(k,j,parent,size);
+                }
+                active[j] =1;
+                
+            }
+            
+            vector<int>leader;
+            
+            for(int l : i.second){
+                leader.push_back(find(l,parent));
+            }
+            
+            for(int h=0; h<leader.size();h++)
+                cout<<leader[h];
+            cout<<endl;
+            
+            sort(leader.begin(),leader.end());
+            
+  
+            int si = leader.size();
+            for(int z=0; z<si;z++){
+                
+                int curr = leader[z];
+                long long count =0;
+                
+                while(z<si && curr == leader[z] ) {
+                    count++;
+                    z++;
+                }
+                z--;
+                
+                result += (count * (count -1))/2;
+                
+            }
+            
+            
+        }
+
+        return result;
+        
+        
     }
 };
 
-class Solution {
-public:
-    int numberOfGoodPaths(vector<int>& vals, vector<vector<int>>& edges) {
-        int n = vals.size();
-        vector<vector<int>> g(n);
-        for (auto i : edges) {
-            g[i[0]].push_back(i[1]);
-            g[i[1]].push_back(i[0]);
-        }
-        
-        map<int, vector<int>> val_to_nodes;
-        for (int j = 0; j < n; j ++) val_to_nodes[vals[j]].push_back(j);
-        
-        DSU dsu(n);
-        int result = n;
-        vector<bool> is_active(n);
-        
-        for (auto [_, indexes] : val_to_nodes) {
-            for (auto i : indexes) {
-                for (auto child: g[i]) {
-                    if (is_active[child]) dsu.merge (i, child);
-                }
-                is_active[i] = true;
-            }
-            
-            vector<int> leaders;
-            for (auto i : indexes) leaders.push_back(dsu.Leader(i));
-            sort (leaders.begin(), leaders.end());
-                        
-            int sz = leaders.size();
-            for (int j = 0; j < sz; j ++) {
-                long long cnt = 0;
-                int cur = leaders[j];
-                
-                while (j < sz && leaders[j] == cur) j ++, cnt ++;
-                j --;
-                
-                result += (cnt * (cnt - 1))/2;
-            }
-        }
-        
-        return result;
-    }
-};
+// 0   1,2        
+// 1   0
+// 2   0,3,4
+// 3   2
+// 4   2

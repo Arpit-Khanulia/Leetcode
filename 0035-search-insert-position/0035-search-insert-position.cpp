0cb423d96842f4569ecc3d2adefbbea58df644fb
Arpit-Khanulia <88695197+Arpit-Khanulia@users.noreply.github.com>
2023-01-17 09:32:16 -0800

    Time: 3 ms (93.19%), Space: 9.5 MB (95.32%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -5,18 +5,19 @@ public:
         
         int i= 0; 
         int j =nums.size()-1;
+        int mid =0;
         
         
         
         while(i<=j){
             
-            if(target < nums[i])return i;
-            int mid = (i + j)/2;
+            // if(target < nums[i])return i;
+            mid = (i + j)/2;
             
             
             if(target == nums[mid]) return mid;
             
-            if(i == j && target > nums[i]) return i+1;
+            // if(i == j && target > nums[i]) return i+1;
             
             
             if(target < nums[mid]) j = mid-1;
@@ -24,7 +25,8 @@ public:
             if(target > nums[mid]) i = mid +1;
         }
         
-        return -1;
+        if(target> nums[mid]) return mid +1;
+        else return mid;
         
     }
 };

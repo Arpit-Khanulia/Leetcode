12b6b78868bbb15bedd4b89d2d3734a51fb8cf03
Arpit-Khanulia <88695197+Arpit-Khanulia@users.noreply.github.com>
2023-01-18 22:10:09 -0800

    Time: 77 ms (54.24%), Space: 33 MB (5.58%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -2,61 +2,30 @@ class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
         
-//         int count =0;
-//         int sum = 0;
-//         for(int i=0 ; i<nums.size();i++){
-//             for(int j= i ; j<nums.size();j++){
-                
-//                 sum = sum + nums[j];
-//                 if(sum%k == 0) count++;
-//             }
-//             sum =0;
-//         }
-        
-//         return count;
-        
         int size = nums.size();
         vector<int>psm(size+1);
+        map<int,int>m;
         int sum =0;
         psm[0] = 0;
+        int count =0;
+        
+        
         for(int i=0 ; i<size; i++){
             
             sum = sum + nums[i];
-            
             int rem = sum%k; 
             if(rem<0) rem = rem + k;
             psm[i+1] = rem;
         }
         
-        map<int,int>m;
-        
         for(int i: psm)
             m[i]++;
         
-        int count =0;
+        
         for(auto i: m)
-            if(i.second >1)
-            {
-                count = count + (i.second * (i.second -1))/2;
-            }
-        
-        return count;
-
-    // int n = nums.size();
-    // int count = 0;
-    // unordered_map<int, int> rem_freq{{0, 1}};
-    // int cum_sum = 0;
-    // for (int i = 0; i < n; i++) {
-    //     cum_sum += nums[i];
-    //     int rem = cum_sum % k;
-    //     if (rem < 0) rem += k;
-    //     if (rem_freq.count(rem)) count += rem_freq[rem];
-    //     if (rem_freq.count(rem)) rem_freq[rem]++;
-    //     else rem_freq[rem] = 1;
-    // }
-    // return count;
-
-        
+            if(i.second >1) count = count + (i.second * (i.second -1))/2;
+            
+        return count;        
         
     }
 };

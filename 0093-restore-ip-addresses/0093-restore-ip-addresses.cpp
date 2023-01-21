8a175303d180ad334364047260aaa3389afc7d5a
Arpit-Khanulia <88695197+Arpit-Khanulia@users.noreply.github.com>
2023-01-20 20:31:53 -0800

    Time: 21 ms (5.14%), Space: 6.9 MB (24.93%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     
     int check(string &ds, int pdot){
-        
-        cout<<"this is pdot "<<pdot<<endl;
+
         string sub = string(ds.begin() + pdot,ds.end());
         
         if(sub[0] == '0' && sub.size()>1) return 0;
@@ -20,27 +19,19 @@ public:
     void solve(string &s, string ds, vector<string>&ans,int i,int pdot,int dcount){
         
         if(i == s.size()){
-
-            cout<<"this is ds "<<ds<<endl;
             if(check(ds,pdot) && dcount == 3) ans.push_back(ds);
             return ;
         }
         
         ds.push_back(s[i]);
         
-        if(check(ds,pdot)){
-            
-            solve(s,ds,ans,i+1,pdot,dcount);
-        }
+        if(check(ds,pdot)) solve(s,ds,ans,i+1,pdot,dcount);
         
         if(ds.size() - pdot >1 && dcount <=2)
         {
-            cout<<"This is the size -> "<<ds.size() - pdot+1<<endl;
             int size = ds.size()-1;
             ds[size] = '.';
-            dcount++;
-            solve(s,ds,ans,i,size+1,dcount);
-     
+            solve(s,ds,ans,i,size+1,++dcount);
         }
 
         

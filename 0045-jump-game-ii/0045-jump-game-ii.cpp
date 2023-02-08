d5e6446e5b1243a351923faed276a402304c436c
Arpit-Khanulia <88695197+Arpit-Khanulia@users.noreply.github.com>
2023-02-08 13:34:12 +0530

    Time: 15 ms (84.34%), Space: 16.6 MB (56.17%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -7,6 +7,7 @@ public:
         int current =0;
         for(int i=0; i<nums.size()-1;i++){
             
+            if(current >= nums.size()-1) break;
             farthest = max(farthest,i+nums[i]);
             if(i == current){
                 current = farthest;

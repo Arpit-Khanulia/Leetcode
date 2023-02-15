d3b62ed0f24a781260e58f64a7a67b34fcf88c8f
Arpit-Khanulia <88695197+Arpit-Khanulia@users.noreply.github.com>
2023-02-15 14:12:13 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (38.95%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-    // Function to add two binary numbers represented as strings
     string addBinary(string a, string b) {
-        // Initialize two pointers to traverse the binary strings from right to left
-        int i = a.length()-1;
-        int j = b.length()-1;
-        string ans;
-        int carry = 0;
         
-        // Loop until both pointers have reached the beginning of their respective strings and there is no carry-over value left
-        while(i >= 0 || j >= 0 || carry) {
-            // Add the current binary digit in string a, if the pointer is still within bounds
-            if(i >= 0) {
-                carry += a[i] - '0';
-                i--;
-            }
+        
+        int c= 0;
+        string ans;
+        
+        int i=a.size()-1;
+        int j=b.size()-1;
+        
+        while(i>=0 || j>=0 || c ){
             
-            // Add the current binary digit in string b, if the pointer is still within bounds
-            if(j >= 0) {
-                carry += b[j] - '0';
-                j--;
-            }
+            int sum= 0;
+            if(i>=0)
+            sum += a[i] -'0';
             
-            // Calculate the next binary digit in the result by taking the remainder of the sum divided by 2
-            ans += (carry % 2 + '0');
+            if(j>=0)
+            sum += b[j] -'0';
             
-            // Calculate the next carry-over value by dividing the sum by 2
-            carry = carry / 2;
+            sum =  sum + c;
+            string q = to_string(sum%2);
+            ans += q;
+            c = sum/2;
+            i--;
+            j--;
         }
         
-        // Reverse the result and return it as a string
-        reverse(ans.begin(), ans.end());
+        reverse(ans.begin(),ans.end());
         return ans;
+        
     }
 };

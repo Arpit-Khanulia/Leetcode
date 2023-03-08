b81f6d2b8af3daaa7607bec45a77be9e3deab04f
Arpit-Khanulia <88695197+Arpit-Khanulia@users.noreply.github.com>
2023-03-09 00:24:20 +0530

    Time: 46 ms (77.01%), Space: 19 MB (45.12%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
-    long long getHoursToEatAll(vector<int>&piles, int bananasPerHour)
-    {
-        long long totalHours = 0;
-        for (int i = 0; i < piles.size(); i++)
-        {
-            int hoursToEatPile = ceil(piles[i] / (double)bananasPerHour);
-            totalHours += hoursToEatPile;
-        }
-        return totalHours;
-    }
-    int minEatingSpeed(vector<int>& piles, int targetHours)
-    {
-        int low = 1, high = *(max_element(piles.begin(), piles.end()));
-        int ans = -1;
-        //================================================================
-        while(low <= high)
-        {
-            int mid = low + (high - low) / 2;
-            long long hoursToEatAll = getHoursToEatAll(piles, mid);
+    
+    bool isvalid(vector<int>&piles, int n,int h){
+        
+        long long hours =0;
+        for(int i: piles){
             
-            if (hoursToEatAll <= targetHours)
-            {
-                ans = mid; //record the answer (this is the best we could record till curr step)
-                high = mid - 1;
-            }
-            else low = mid + 1;
+            if(i%n == 0) hours+=i/n;
+            else hours+= (i/n) +1;
+            
         }
-        //=================================================================
+        
+        if(hours<= h) return 1;
+        return 0;
+        
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+        int low = 1;
+        int high = 0;
+        int ans = 0;
+        
+        for(int i: piles) high = max(high,i);
+        
+        
+        while(low<= high){
+            int mid = (high + low)/2;
+            // int mid = low + (high-mid)/2;
+            if(isvalid(piles,mid,h)){
+                
+                ans = mid;
+                high = mid -1;
+            }
+            else low = mid +1;
+        }
+        
         return ans;
+        
     }
 };

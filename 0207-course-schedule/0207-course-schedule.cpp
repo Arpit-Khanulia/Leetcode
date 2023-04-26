88e2f11bf142c263906cbe1393ce3722543ee44d
Arpit-Khanulia <88695197+Arpit-Khanulia@users.noreply.github.com>
2023-04-26 19:05:50 +0530

    Time: 23 ms (64.19%), Space: 13 MB (97.54%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,34 +2,29 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         
-        
         vector<int>adj[numCourses];
         
         for(int i=0; i<prerequisites.size();i++){
             
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
-
         
-        vector<int>indegree(numCourses);
-        
-        for(int i=0; i<numCourses; i++){
-            
-            for(auto j: adj[i])
-                indegree[j]++;
-        }
-        
-        
+        vector<int>ans;
+        vector<int>indgree(numCourses);
         queue<int>q;
-        vector<int>topo;
         
-        for(int i=0; i<numCourses;i++)
-        {
-            if(indegree[i] == 0)
-                q.push(i);
+        for(int i=0; i<numCourses;i++){
+            
+            for(int j: adj[i]){
+                
+                indgree[j]++;
+            }
             
         }
-            
+        
+        for(int i=0; i<indgree.size();i++){
+            if(indgree[i] == 0) q.push(i);
+        }
         
         
         while(!q.empty()){
@@ -37,19 +32,17 @@ public:
             int node = q.front();
             q.pop();
             
-            topo.push_back(node);
+            ans.push_back(node);
             
-            for(int i: adj[node])
-            {
-                indegree[i]--;
-                if(indegree[i] == 0)
-                    q.push(i);
+            for(int i: adj[node]){
+                
+                indgree[i]--;
+                if(indgree[i] == 0) q.push(i);
             }
+            
         }
         
-        if(topo.size() == numCourses) return 1;
+        if(ans.size() == numCourses) return 1;
         return 0;
-        
-        
     }
 };

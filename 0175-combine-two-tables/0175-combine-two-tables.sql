a27f4fa410b9357629eca85e195c7823252ef826
Arpit-Khanulia <88695197+Arpit-Khanulia@users.noreply.github.com>
2023-08-25 10:01:42 +0530

    Time: 1072 ms (22.50%), Space: 0B (100.00%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,4 +1,11 @@
 # Write your MySQL query statement below
 
 
-select firstName, lastName,city,state from Person p left outer join Address a on p.personID = a.personID 
+# select firstName, lastName,city,state from Person p left outer join Address a on p.personID = a.personID 
+
+SELECT 
+    p.firstName,
+    p.lastName,
+    (SELECT a.city FROM Address a WHERE a.personId = p.personId) AS city,
+    (SELECT a.state FROM Address a WHERE a.personId = p.personId) AS state
+FROM Person p;

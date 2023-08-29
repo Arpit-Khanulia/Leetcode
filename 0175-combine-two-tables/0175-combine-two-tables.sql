d008b86dfccbb2a9d8ae43d3539224f1340c6f62
Arpit-Khanulia <88695197+Arpit-Khanulia@users.noreply.github.com>
2023-08-29 09:11:58 +0530

    Time: 888 ms (58.50%), Space: 0B (100.00%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -3,9 +3,13 @@
 
 # select firstName, lastName,city,state from Person p left outer join Address a on p.personID = a.personID 
 
-SELECT 
-    p.firstName,
-    p.lastName,
-    (SELECT a.city FROM Address a WHERE a.personId = p.personId) AS city,
-    (SELECT a.state FROM Address a WHERE a.personId = p.personId) AS state
-FROM Person p;
+# SELECT 
+#     p.firstName,
+#     p.lastName,
+#     (SELECT a.city FROM Address a WHERE a.personId = p.personId) AS city,
+#     (SELECT a.state FROM Address a WHERE a.personId = p.personId) AS state
+# FROM Person p;
+
+
+select firstName, lastName , city , state 
+from Person p left join Address a on p.personID = a.personID

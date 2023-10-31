f9364c6e50a782d240d1e293261f12e58820dfc9
Arpit-Khanulia <88695197+Arpit-Khanulia@users.noreply.github.com>
2023-10-31 08:29:19 +0530

    Time: 3 ms (64.26%), Space: 11.3 MB (50.73%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    
+    int binarysearch(vector<int>&nums, int target,int s ,int e){
+        
+        while(s<=e){
+            
+            int mid = s + (e-s)/2;
+            
+            if(nums[mid]< target) s = mid+1;
+            else if( nums[mid] > target) e = mid-1;
+            else return mid;
+        }
+        
+        return -1;
+    }
+    
+    int findmin(vector<int>&nums){
+        
+        int n = nums.size();
+        int s= 0;
+        int e = n-1;
+        
+        if(n == 1) return 0;
+        
+        while(s<=e){
+            
+            int mid  = s + (e-s)/2;
+            
+            int prev = (mid -1+n)%n;
+            int next = (mid +1)%n;
+            
+            if(nums[mid] < nums[prev] and nums[mid] < nums[next]) return mid;
+            if(nums[mid] < nums[n-1]) e = mid-1;
+            else s = mid+1;
+        }
+        
+        return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        
+        int min = findmin(nums);
+        cout<<nums[min]<<endl;
+        
+        int left = binarysearch(nums,target, 0,min-1);
+        int right = binarysearch(nums,target,min,nums.size()-1);
+        
+        if(left != -1) return left;
+        else if(right != -1) return right;
+        else return -1;
+        
+        return 0;
+        
+    }
+};

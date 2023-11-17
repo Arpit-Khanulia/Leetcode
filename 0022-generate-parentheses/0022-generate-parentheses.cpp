7674599d74cf14beb566bdda06d8561186efc900
Arpit-Khanulia <88695197+Arpit-Khanulia@users.noreply.github.com>
2023-11-17 09:06:11 +0530

    Time: 6 ms (26.71%), Space: 11.8 MB (87.19%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
-//  0<3 (, 1<3 ( , 2<3(, 0 <3 ), 1<3 ) , 2<3 ),      
-    void find(vector<string>&ans,string &ds, int n, int open , int close){
+    
+    void generate(int open ,int close, string & ds, vector<string>&ans){
         
         
-        if(open ==n and  close == n){
+        if(open == 0 and  close == 0){
             
             ans.push_back(ds);
             return;
         }
         
-        if(open < n){
+        if(open>0){
+            
             ds.push_back('(');
-            find(ans,ds,n,open+1,close);
-            ds.pop_back(); 
+            generate(open-1,close,ds,ans);
+            ds.pop_back();          
+        }
+
+        if(close>open){
+            
+            ds.push_back(')');
+            generate(open,close-1,ds,ans);
+            ds.pop_back();
         }
         
-        if(close<n and close < open ){
-            ds.push_back(')');
-            find(ans,ds,n,open,close+1);
-            ds.pop_back();         
-        }  
-
+        
     }
     vector<string> generateParenthesis(int n) {
         
-        vector<string>ans;
         string ds;
-        find(ans,ds,n,0,0);
+        vector<string>ans;
+        generate(n,n,ds,ans);
         return ans;
-        
     }
 };
